83faf9de94dac57798cb7275f45e7d73be55d621
Milam <charles.milam@gmail.com>
2015-01-14 23:51:46 -0600

    Modify to add controller and view

--- public/js/quizzy.js
+++ public/js/quizzy.js
@@ -2,55 +2,80 @@
 
   window.QuizzyApp = {}
 
-  // View-Model
-  QuizzyApp.vm = {
-    quizzes: m.prop([])
+  // view-model
+  QuizzyApp.vm = (function() {
+    var vm = {}
+
+    vm.init = function() {
+      // list of quizzes
+      vm.quizzes = m.prop(quizzes[0])
+    }
+      return vm
+  }())
+
+  // the controller
+  QuizzyApp.controller = function() {
+    QuizzyApp.vm.init()
   }
 
-// the data
-var quizzes = [{
-            id: 100,
-            question: "How much wood would a woodchuck chuck?",
-            responses: [
-                                "3",
-                                "42",
-                                "An inordinate amount",
-                                "You woodchucks get away from my wood pile!"
-                                ],
-            answer: 1
-        },
-        {
-            id: 101,
-            question: "If it looks like a duck, and quacks like a duck?",
-            responses: [
-                                "It's a duck",
-                                "Aflac!",
-                                "42",
-                                "You woodchucks get away from my wood pile!"
-                                ],
-            answer: 2
-        },
-        {
-            id: 102,
-            question: "Knock, knock?",
-            responses: [
-                                "42",
-                                "Who's there?",
-                                "Dave?",
-                                "Dave's not here!"
-                                ],
-            answer: 0
-        },
-        {
-            id: 103,
-            question: "Which is not one of the three laws of robotics?",
-            responses: [
-                                "A robot may not injure a human being or, through inaction, allow a human being to come to harm.",
-                                "A robot must obey orders given it by human beings except where such orders would conflict with the First Law.",
-                                "A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.",
-                                "Destroy all humans!"
-                                ],
-            answer: 3
-        }
-    ])
+  // the view
+  QuizzyApp.view = function() {
+    return m("html", [
+        m("body", [
+          m("div", [
+            m("label", QuizzyApp.vm.quizzes().question),
+            m("br"),
+            m("input[type=radio]", {checked: false}),
+            m("label", QuizzyApp.vm.quizzes().responses[3])
+          ])
+        ])
+      ])
+  }
+
+  // the data
+  var quizzes = [{
+              id: 100,
+              question: "How much wood would a woodchuck chuck?",
+              responses: [
+                                  "3",
+                                  "42",
+                                  "An inordinate amount",
+                                  "You woodchucks get away from my wood pile!"
+                                  ],
+              answer: 1
+          },
+          {
+              id: 101,
+              question: "If it looks like a duck, and quacks like a duck?",
+              responses: [
+                                  "It's a duck",
+                                  "Aflac!",
+                                  "42",
+                                  "You woodchucks get away from my wood pile!"
+                                  ],
+              answer: 2
+          },
+          {
+              id: 102,
+              question: "Knock, knock?",
+              responses: [
+                                  "42",
+                                  "Who's there?",
+                                  "Dave?",
+                                  "Dave's not here!"
+                                  ],
+              answer: 0
+          },
+          {
+              id: 103,
+              question: "Which is not one of the three laws of robotics?",
+              responses: [
+                                  "A robot may not injure a human being or, through inaction, allow a human being to come to harm.",
+                                  "A robot must obey orders given it by human beings except where such orders would conflict with the First Law.",
+                                  "A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.",
+                                  "Destroy all humans!"
+                                  ],
+              answer: 3
+          }
+      ]
 })()
