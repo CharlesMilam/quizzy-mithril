64e61d544d7974fd642abdbb2a31fa6ba798ef90
Milam <charles.milam@gmail.com>
2015-01-14 22:17:11 -0600

    Add view-model and data

---
+++ public/js/quizzy.js
@@ -0,0 +1,56 @@
+(function () {
+
+  window.QuizzyApp = {}
+
+  // View-Model
+  QuizzyApp.vm = {
+    quizzes: m.prop([])
+  }
+
+// the data
+var quizzes = [{
+            id: 100,
+            question: "How much wood would a woodchuck chuck?",
+            responses: [
+                                "3",
+                                "42",
+                                "An inordinate amount",
+                                "You woodchucks get away from my wood pile!"
+                                ],
+            answer: 1
+        },
+        {
+            id: 101,
+            question: "If it looks like a duck, and quacks like a duck?",
+            responses: [
+                                "It's a duck",
+                                "Aflac!",
+                                "42",
+                                "You woodchucks get away from my wood pile!"
+                                ],
+            answer: 2
+        },
+        {
+            id: 102,
+            question: "Knock, knock?",
+            responses: [
+                                "42",
+                                "Who's there?",
+                                "Dave?",
+                                "Dave's not here!"
+                                ],
+            answer: 0
+        },
+        {
+            id: 103,
+            question: "Which is not one of the three laws of robotics?",
+            responses: [
+                                "A robot may not injure a human being or, through inaction, allow a human being to come to harm.",
+                                "A robot must obey orders given it by human beings except where such orders would conflict with the First Law.",
+                                "A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.",
+                                "Destroy all humans!"
+                                ],
+            answer: 3
+        }
+    ])
+})()
